dbe2779c52ab6918f42464c32e8eaa9e8c9cef4e
xogur <snsk656@naver.com>
2025-06-28 18:41:34 +0900

    16

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import './index.css';
-import { FaBeer } from 'react-icons/fa';
+import Products from './components/Products'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div
-      className='flex items-center justify-center h-screen bg-gray-800 text-white
-       text-4xl font-bold'>
-        Welcome <FaBeer />
-    </div>
+    <>
+      <Products />
+    </>
   )
 }
 
