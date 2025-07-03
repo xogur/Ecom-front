d886469b2ee6e0576f3c146a43b61ef4020ee3c4
xogur <snsk656@naver.com>
2025-07-03 21:40:58 +0900

    57

--- src/components/checkout/Checkout.jsx
+++ src/components/checkout/Checkout.jsx
@@ -1,5 +1,8 @@
 import { Step, StepLabel, Stepper } from '@mui/material';
 import React, { useState } from 'react'
+import AddressInfo from './AddressInfo';
+
+
 
 const Checkout = () => {
     const [activeStep, setActiveStep] = useState(0);
@@ -20,6 +23,9 @@ const Checkout = () => {
                 </Step>
             ))}
         </Stepper>
+        <div className='mt-5'>
+            {activeStep === 0 && <AddressInfo />}
+        </div>
     </div>
   );
 }
