7aeda4b6e952ce872b2c918bf74bc06c303d8232
xogur <snsk656@naver.com>
2025-07-03 15:43:50 +0900

    52

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Contact from './components/Contact'
 import { Toaster } from 'react-hot-toast'
 import Cart from './components/cart/Cart'
 import LogIn from './components/auth/LogIn'
+import PrivateRoute from './components/PrivateRoute'
 
 
 function App() {
@@ -24,7 +25,9 @@ function App() {
           <Route path='/about' element={ <About />}/>
           <Route path='/contact' element={ <Contact />}/>
           <Route path='/cart' element={ <Cart />}/>
-          <Route path='/login' element={ <LogIn />}/>
+          <Route path='/' element={<PrivateRoute publicPage />}>
+            <Route path='/login' element={ <LogIn />}/>
+          </Route>
         </Routes>
       </Router>
       <Toaster position='bottom-center'/>
