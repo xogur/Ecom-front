1bf93448377466b772ec105a16a0ef83f374630b
xogur <snsk656@naver.com>
2025-10-04 12:17:14 +0900

    cicd test

--- src/api/api.js
+++ src/api/api.js
@@ -1,18 +1,18 @@
-import axios from "axios";
-
-const api = axios.create({
-    baseURL: `${import.meta.env.VITE_BACK_END_URL}/api`,
-    withCredentials: true,
-});
-
-export default api;
-
 // import axios from "axios";
 
 // const api = axios.create({
-//   baseURL: "/api",       // 절대 URL( http://IP:8080 ) 절대 금지!
-//   withCredentials: true, // 쿠키 인증이면 true
-//   timeout: 15000,
+//     baseURL: `${import.meta.env.VITE_BACK_END_URL}/api`,
+//     withCredentials: true,
 // });
 
 // export default api;
+
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "/api",       // 절대 URL( http://IP:8080 ) 절대 금지!
+  withCredentials: true, // 쿠키 인증이면 true
+  timeout: 15000,
+});
+
+export default api;
